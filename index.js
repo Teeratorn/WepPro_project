8d434585c1da6081ec4c2200bbfe46e8cddd8019
Teeratorn <64070165@it.kmitl.ac.th>
2023-02-28 15:11:10 +0700

    add index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        games: [
+            {
+                name: "test name",
+                dev: "test dev",
+                price: "test price",
+                img: 'https://c4.wallpaperflare.com/wallpaper/591/474/400/anime-aokana-four-rhythm-across-the-blue-mashiro-arisaka-wallpaper-preview.jpg',
+            },
+            {
+                name: "test name",
+                dev: "test dev",
+                price: "test price",
+                img: 'https://c4.wallpaperflare.com/wallpaper/919/984/987/anime-aokana-four-rhythm-across-the-blue-mashiro-arisaka-wallpaper-preview.jpg',
+            },
+        ]
+    }
+})
