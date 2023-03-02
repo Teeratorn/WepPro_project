bdb9eb5c70a99d8d01380503ed4cff8c587f2ce2
Teeratorn <64070165@it.kmitl.ac.th>
2023-03-03 02:33:53 +0700

    add finalfantasyxiii.html

--- script.js
+++ script.js
@@ -34,6 +34,22 @@ var app = new Vue({
                 url: "darksoul3.html",
                 reviews: [{ name: 'Gabriel', text: 'Even the stupid mob can oneshot me.' }],
             },
+            {
+                name: "Atelier Ryza",
+                dev: "KOEI TECMO GAMES CO., LTD.",
+                price: "1933",
+                img: 'https://upload.wikimedia.org/wikipedia/en/1/13/Atelier_Ryza_decalless_cover_art.jpg',
+                url: "atelierryza.html",
+                reviews: [{ name: 'Sarah', text: 'The alchemy system is so good. I think want to be an alchemist.' }],
+            },
+            {
+                name: "FINAL FANTASY® XIII",
+                dev: "Square Enix",
+                price: "507",
+                img: 'https://upload.wikimedia.org/wikipedia/en/4/47/Final_Fantasy_XIII_EU_box_art.jpg',
+                url: "finalfantasyxiii.html",
+                reviews: [{ name: 'Helen', text: 'Lightning is so cool. I think I\'m in love.' }],
+            },
         ],
         gameName: null,
     },
@@ -44,10 +60,11 @@ var app = new Vue({
     },
     methods: {
         goHome() {
-            location.replace("index.html")
+            this.getJSON()
+            location.href = "index.html"
         },
         goGame(url) {
-            this.getJSON()
+            this.setJSON()
             location.replace(url)
         },
         setJSON() {
@@ -63,50 +80,14 @@ var app = new Vue({
 var app1 = new Vue({
     el: '#app1',
     data: {
-        games: [
-            {
-                name: "Elden ring",
-                dev: "FromSoftware Inc.",
-                price: "1790",
-                img: 'https://image.api.playstation.com/vulcan/ap/rnd/202108/0410/np2Eb60bDep9fDWtqNNSzqZI.png',
-                url: "eldenring.html",
-                reviews: [{ name: 'Bob', text: 'This game is so good. I\'m playing this for 5 hours now.' }],
-            },
-            {
-                name: "Hogwarts Legacy",
-                dev: "Avalance Software",
-                price: "1590",
-                img: 'https://image.api.playstation.com/vulcan/ap/rnd/202208/0921/dR9KJAKDW2izPbptHQbh3rnj.png',
-                url: "hogwartslegacy.html",
-                reviews: [{ name: 'Gorge', text: 'Haha funny wizard game.' }],
-            },
-            {
-                name: "Cyberpunk 2077",
-                dev: "CD PROJEKT RED",
-                price: "1799",
-                img: 'https://image.api.playstation.com/vulcan/ap/rnd/202111/3013/cKZ4tKNFj9C00giTzYtH8PF1.png',
-                url: "cyberpunk.html",
-                reviews: [{ name: 'Michael', text: 'This game made me feel things.' }],
-            },
-            {
-                name: "Dark soul 3",
-                dev: "FromSoftware Inc.",
-                price: "1500",
-                img: 'https://image.api.playstation.com/cdn/UP0700/CUSA03388_00/v8JlD8KcQUtTqaLBmpFnj1ESRR5zMkLk.png',
-                url: "darksoul3.html",
-                reviews: [{ name: 'Gabriel', text: 'Even the stupid mob can oneshot me.' }],
-            },
-        ],
+        games: app.games,
+
         name: null,
         reviewText: null,
     },
     methods: {
         goHome() {
-            location.href = "index.html"
-        },
-        goGame(url) {
-            this.getJSON()
-            location.href = url
+            app.goHome()
         },
         addReview(game) {
             game.reviews.push({ name: this.name, text: this.reviewText })
