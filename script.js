29dd4b7305dd1608a60501028c1c2b7c27debed9
Teeratorn <64070165@it.kmitl.ac.th>
2023-03-02 15:54:15 +0700

    Update eldenring.html

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ var app = new Vue({
                 price: "1790",
                 img: 'https://image.api.playstation.com/vulcan/ap/rnd/202108/0410/np2Eb60bDep9fDWtqNNSzqZI.png',
                 url: "eldenring.html",
+                reviews: [{ name: 'Bob', text: 'This game is so good. I\'m playing this for 5 hours now.' }],
             },
             {
                 name: "Hogwarts Legacy",
@@ -15,8 +16,11 @@ var app = new Vue({
                 price: "1590",
                 img: 'https://image.api.playstation.com/vulcan/ap/rnd/202208/0921/dR9KJAKDW2izPbptHQbh3rnj.png',
                 url: "hogwartslegacy.html",
+                reviews: [],
             },
-        ]
+        ],
+        name: null,
+        reviewText: null,
     },
     methods: {
         goHome() {
@@ -25,5 +29,8 @@ var app = new Vue({
         goGame(url) {
             location.replace(url)
         },
+        addReview(game) {
+            game.reviews.push({ name: this.name, text: this.reviewText })
+        }
     }
 })
